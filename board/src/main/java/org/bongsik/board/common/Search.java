c1cf7a310596ac4c0673b30c9fc2fe0e454439b4
bongsik <djaqhdtlr@hanmail.net>
2020-05-26 23:10:58 +0900

    검색기능.

--- board/src/main/java/org/bongsik/board/common/Search.java
+++ board/src/main/java/org/bongsik/board/common/Search.java
@@ -1,8 +1,10 @@
 package org.bongsik.board.common;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode (callSuper = false)
 public class Search extends Pagination{
 	
 	private String searchType;
